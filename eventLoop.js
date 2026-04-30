4bd785a04a3087a34d0e95bc22cbc7e29065afa6
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2026-04-30 23:58:15 +0530

    Asynchronous JavaScript & EVENT LOOP

---
+++ eventLoop.js
@@ -0,0 +1,9 @@
+console.log("start")
+setTimeout(function cbt() {
+    console.log("cbt timeout")
+}, 5000);
+fetch("https://api.netflix.com")
+    .then(function cbf() {
+        console.log("cbf netflix")
+    })
+console.log("end")
